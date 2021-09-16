d462eaaae27b65fb33d4561bfb134d379e88eb9c
Jeeva-1701 <89633224+Jeeva-1701@users.noreply.github.com>
2021-09-16 13:25:04 +0530

    Create Areaofrectangle.java

---
+++ Areaofrectangle.java
@@ -0,0 +1,8 @@
+public class Areaofrectangle{  
+    public static void main(String args[])  
+    {  
+    int a=5,b=7;  //a=width and b=height
+    int area=a*b;  //formulae of the area of rectangle is area=width*height
+        System.out.println("Area Of rectangle="+area);  
+     }  
+}  
